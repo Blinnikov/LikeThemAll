3973456148b3a199222a93076055fe014783dd9d
Igor Blinnikov <igor.blinnikov@gmail.com>
2014-12-02 23:17:44 +0300

    Mongo support
    - Added C# driver package
    - Commented ASP.NET Core framework because of driver doesn't support it

---
+++ src/LikeThemAll.Web/Controllers/BookController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Mvc;
+using MongoDB.Driver;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LikeThemAll.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            return View();
+        }
+    }
+}
